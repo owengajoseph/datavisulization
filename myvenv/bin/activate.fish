b18f8205809cf4a3bdbf38e9641f984fae96f9db
owenga <owengajoseph54@gmail.com>
2024-11-11 18:58:04 +0300

    new files

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/user426/git/datavisulization/myvenv"
+set -gx VIRTUAL_ENV "/home/use426/git/datavisulization/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
